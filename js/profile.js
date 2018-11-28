6f6a1fb0e58ad8b7b1ed3aced5b8a3f2ddff1cc4
Zerones <43774584+Zerones@users.noreply.github.com>
2018-11-28 12:17:47 +0100

    Profil siden er ikke helt ferdig ennå, men arrayen fra database.js er intigrert.

--- js/profile.js
+++ js/profile.js
@@ -1,20 +1,12 @@
-var PhoneCP;
-var EmailCP;
-var PickupCP;
-var statusCP;
-var phoneNumberP;
-var EmailP;
-var PickupP;
-var statusP;
-var phoneT;
-var emailT;
-var pickupT;
-var statusT;
-var passT;
-var passTC;
-var brukerNavn;
-var nyProfile;
-var brukerNavnT;
+var toptekstenbragehemmeligvar = `
+    
+<div class="backtop"></div>        
+<div class="backheadpassreq">   Profile    </div>
+<div onclick="showProPage()" class="backbuttonsymbol">&lt;&lt</div></a>
+<div onclick="showProPage()" class="backbutton"></div></a>       
+<div class="lele2"><h2></h2></div>    
+<div class="backgroundkaos"></div>      
+<div class="profileBilde"></div>`;
 
 
 
@@ -26,52 +18,37 @@ function showProPage()
 
     if(ny = !undefined)
     {
-        mainContentDiv.innerHTML = `
-  
-        <div class="backtop"></div>        
-        <div class="backheadpassreq">   Profile    </div>
-        <div onclick="showMenu()" class="backbuttonsymbol">&lt;&lt</div></a>
-        <div onclick="showMenu()" class="backbutton"></div></a>
-                   
-            <div class="lele2"><h2></h2></div>    
-            <div class="backgroundkaos"></div>      
-            <div class="profileBilde"></div>
+        mainContentDiv.innerHTML =  toptekstenbragehemmeligvar + `
+
             <div class="ProfileBoks">  
                     <div id="Navn" class="profileNavn">` + pro.username + `</div>
                     <div class="profile1">Phone Number<br><div id="nummer" class="profileInfo">` + pro.phoneNumber + `</div></div>
                     <div class="profile2">Email <br><div id="mail" class="profileInfo">` + pro.email + `</div></div>
                     <div class="profile3">Pick up Point <br><div id="point" class="profileInfo">` + pro.pickPoint + `</div></div>
                     <div class="profile4">Driver Status<br><div id="status" class="profileInfo">` + pro.status + `</div></div>
-                    <div class="profile6"> Passenger?: <div class="profileInfo"> `+ pro.role[1] +`</div></div>
-                    <div class="profile7"> Driver?: <div class="profileInfo">`+ pro.role[0] +`</div></div>        
-                    <div class="profile5" onclick="profileEdit()">Edit Profile</div>
+                    <div class="profile5"> Passenger?: <div class="profileInfo"> `+ pro.role[1] +`</div></div>
+                    <div class="profile6"> Driver?: <div class="profileInfo">`+ pro.role[0] +`</div></div>        
+                    <div class="profile7" onclick="profileEdit()">Edit Profile</div>
             </div>`
         ;
     }
     else
     {
-        mainContentDiv.innerHTML = `
+        mainContentDiv.innerHTML = toptekstenbragehemmeligvar + `
 
-        <div class="backtop"></div>        
-        <div class="backheadpassreq">   Profile    </div>
-        <div onclick="showMenu()" class="backbuttonsymbol">&lt;&lt</div></a>
-        <div onclick="showMenu()" class="backbutton"></div></a>
-    
-        <div class="lele2"><h2></h2></div>    
-        <div class="backgroundkaos"></div>      
-        <div class="profileBilde"></div>
         <div class="ProfileBoks">  
             <div id="Navn" class="profileNavn">` + ny.username + `</div>
             <div class="profile1">Phone Number<br><div id="nummer" class="profileInfo">` + ny.phoneNumber + `</div></div>
             <div class="profile2">Email <br><div id="mail" class="profileInfo">` + ny.email + `</div></div>
             <div class="profile3">Pick up Point <br><div id="point" class="profileInfo">` + ny.pickPoint + `</div></div>
             <div class="profile4">Driver Status<br><div id="status" class="profileInfo">` + ny.status + `</div></div>
-            <div class="profile6"> Passenger?: <div class="profileInfo"> `+ ny.role[1] +`</div></div>
-            <div class="profile7"> Driver?: <div class="profileInfo">`+ ny.role[0] +`</div></div>
-            <div class="profile5" onclick="profileEdit()">Edit Profile</div>
+            <div class="profile5"> Passenger?: <div class="profileInfo"> `+ ny.role[1] +`</div></div>
+            <div class="profile6"> Driver?: <div class="profileInfo">`+ ny.role[0] +`</div></div>
+            <div class="profile7" onclick="profileEdit()">Edit Profile</div>
         </div>
            
-           `; 
+           `;
+         
     }
 }
 function profileCreate() 
@@ -81,27 +58,38 @@ function profileCreate()
 function profileEdit() 
 {
         var mainContentDiv = document.getElementById('mainContent');
-        mainContentDiv.innerHTML = `
-    
-        <div class="backtop"></div>        
-        <div class="backheadpassreq">   Profile    </div>
-        <div onclick="showProPage()" class="backbuttonsymbol">&lt;&lt</div></a>
-        <div onclick="showProPage()" class="backbutton"></div></a>       
-        <div class="lele2"><h2></h2></div>    
-        <div class="backgroundkaos"></div>      
-        <div class="profileBilde"></div>
-        <div class="ProfileEdit">       
-                <div class="profilePhone"><input type="text" id="phoneT" name="Number" placeholder="Phone Number" style="border: none; text-align: center;"></div>
-                <div class="profileEmail"><input type="text" id="emailT"  name="Mail" placeholder="Email Adress"  style="border: none; text-align: center;"></div>
-                <div class="profilePickup"><input type="text" id="pickupT" name="Point" placeholder="Pick up Point" style="border: none; text-align: center;"></div>
-                <div class="profileStatus"><input type="text" id="statusT" name="Status" placeholder="Driver Status" style="border: none; text-align: center;"></div>
-                <div class="profilePassword"><input type="password" id="passT" name="Password" placeholder="New Password" style="border: none; text-align: center;"></div>
-                <div class="profileConfirm"><input type="password" id="passTC" name="PassAgain" placeholder="New Password Again" style="border: none; text-align: center;"></div>
-                <div class="profileSave" id="feilsokk" onclick="saveP()">Save Changes</div>
-        </div>
+        var lagrefil = toptekstenbragehemmeligvar;
+        lagrefil += '<div class="ProfileBoks" id="boksf">';
+        for(let i = 1; i < 7; i++)
+        {
+            lagrefil += '<div class="profile' + i + '">';
+            if(i == 1) lagrefil += `Phone Number<br><input type="text"  value="">`;
+            else if(i == 2) lagrefil += `Email <br><input type="text"  value="">`;
+            else if(i == 3) lagrefil += `Pick up Point <br><input type="text"  value="">`;
+            else if(i == 4) lagrefil += `Driver Status<br><input type="text"  value="">`;
+            else if(i == 5) lagrefil +=  `Passenger?:<input type="checkbox">`;
+            else if(i == 6) lagrefil += `Driver?:<input type="checkbox">`;
+            lagrefil += `<div class="profileInfo"></div>
+                        </div>`;
+        }
+        lagrefil += `<div class="profilePass1" style="border:none">Password: <input type="password" name="password" id="password"></div>
+                                     <div class="profilePass2" style="border:none">Password again: <input type="password" name="password" id="password"></div>`;
+        lagrefil += `<div class="profile7" onclick="saveProfile()">Save Edits</div>`;
+        lagrefil += '</div>';
+        mainContentDiv.innerHTML = lagrefil;
+        document.getElementById('boksf').style.gridTemplateAreas = `
+                                                                    'profile1 profile1'
+                                                                    'profile2 profile2'
+                                                                    'profile3 profile3'
+                                                                    'profile4 profile4'
+                                                                    'profile5 profile5'
+                                                                    'profile6 profile6'
+                                                                    'profilePass1 profilePass1'
+                                                                    'profilePass2 profilePass2'
+                                                                    'profile7 profile7'
         `;
 }
-function saveP()
+function saveProfile()
 {               
         var mainContentDiv = document.getElementById('mainContent');
         phoneNumberP = PhoneCP;
